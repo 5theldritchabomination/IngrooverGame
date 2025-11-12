67363a8cd9eb1d2f0bf0d49626c91d5a1ea5c595
Thibault <thibault.royer1@ecoles-epsi.net>
2025-11-12 23:30:52 +0100

    MidiFileFinder Update

--- Assets/Scripts/SelectMidiFile/Menu.cs
+++ Assets/Scripts/SelectMidiFile/Menu.cs
@@ -4,16 +4,17 @@ using System.Linq;
 using TMPro;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using UnityEngine.Windows;
+using System;
+using System.Runtime.InteropServices;
 
 public class AddFiles : MonoBehaviour
 {
-public TMP_Dropdown dropdown;
+    public TMP_Dropdown dropdown;
     public Button playButton;
     public Button returnButton;
     public Button addFileButton;
 
-    public string midiFolderPath = "Assets/MidiFiles";
+    public string midiFolderPath = "MidiFiles";
     private static string midiFilePath;
 
     void Start()
@@ -29,65 +30,109 @@ public TMP_Dropdown dropdown;
     {
         dropdown.ClearOptions();
 
-        if (!System.IO.Directory.Exists(midiFolderPath))
-        {
-            Debug.LogWarning("Dossier MIDI introuvable : " + midiFolderPath);
-            return;
-        }
+        if (!Directory.Exists(midiFolderPath)) Directory.CreateDirectory(midiFolderPath);
 
-        string[] midiFiles = System.IO.Directory.GetFiles(midiFolderPath, "*.mid").Concat(System.IO.Directory.GetFiles(midiFolderPath, "*.midi")).ToArray();
+        string[] midiFiles = Directory.GetFiles(midiFolderPath, "*.mid")
+            .Concat(Directory.GetFiles(midiFolderPath, "*.midi")).ToArray();
 
-        var options = midiFiles.Select(file => Path.GetFileNameWithoutExtension(file).Substring(0, Mathf.Min(15, Path.GetFileNameWithoutExtension(file).Length))).ToList();
+        var options = midiFiles.Select(file => Path.GetFileNameWithoutExtension(file)
+            .Substring(0, Mathf.Min(15, Path.GetFileNameWithoutExtension(file).Length))).ToList();
 
         dropdown.AddOptions(options);
     }
 
     void OnDropdownValueChanged(int index)
     {
-        string[] files = System.IO.Directory.GetFiles(midiFolderPath, "*.mid")
-            .Concat(System.IO.Directory.GetFiles(midiFolderPath, "*.midi"))
+        string[] files = Directory.GetFiles(midiFolderPath, "*.mid")
+            .Concat(Directory.GetFiles(midiFolderPath, "*.midi"))
             .ToArray();
 
         if (index < files.Length)
         {
             GameData.midiFilePath = files[index];
-            Debug.Log("Fichier MIDI sélectionné : " + midiFilePath);
+            Debug.Log("Fichier MIDI sélectionné : " + GameData.midiFilePath);
         }
-        var options = files.Select(file => Path.GetFileNameWithoutExtension(file).Substring(0, Mathf.Min(15, Path.GetFileNameWithoutExtension(file).Length))).ToList();
+
+        var options = files.Select(file => Path.GetFileNameWithoutExtension(file)
+            .Substring(0, Mathf.Min(15, Path.GetFileNameWithoutExtension(file).Length))).ToList();
 
         EditButtonStart(options[index]);
-
     }
 
     void LoadMIDI()
     {
         string midiFilePath = "";
-        
+        midiFilePath = OpenFileDialog("Choisir un fichier MIDI", "Fichiers MIDI\0*.mid;*.midi\0Tous les fichiers\0*.*\0");
+        if (string.IsNullOrEmpty(midiFilePath))
+        {
+            Debug.LogWarning("Aucun fichier MIDI sélectionné.");
+            return;
+        }
 
-#if UNITY_EDITOR
-        string midiFolderPath = Path.Combine(Application.dataPath, "MidiFiles");
-        
-        GameData.midiFilePath = UnityEditor.EditorUtility.OpenFilePanel("", midiFolderPath, "mid,midi");
-#endif
-
+        GameData.midiFilePath = midiFilePath;
         Debug.Log("Fichier MIDI sélectionné : " + midiFilePath);
-        var options = midiFilePath.Split(new char []{'/'}).Last().Split(new char []{'.'}) ;
-        EditButtonStart(options[0].Substring(0, Mathf.Min(15, Path.GetFileNameWithoutExtension(options[0]).Length)));
-    }
 
+        var options = Path.GetFileNameWithoutExtension(midiFilePath);
+        EditButtonStart(options.Substring(0, Mathf.Min(15, options.Length)));
+    }
 
     private void EditButtonStart(string midiFilePath)
     {
-        string old = "Lancez le jeu avec : " ;
+        string old = "Lancez le jeu avec : ";
         GameObject.Find("GameStarter").GetComponentInChildren<Text>().text = old + midiFilePath;
     }
-    private void ReturnGame()
+
+    private void ReturnGame() => SceneManager.LoadScene("MainMenu");
+    private void PlayGame() => SceneManager.LoadScene("Game");
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public class OpenFileName
     {
-        SceneManager.LoadScene("MainMenu");
+        public int structSize = 0;
+        public IntPtr dlgOwner = IntPtr.Zero;
+        public IntPtr instance = IntPtr.Zero;
+        public string filter = null;
+        public string customFilter = null;
+        public int maxCustFilter = 0;
+        public int filterIndex = 0;
+        public string file = null;
+        public int maxFile = 0;
+        public string fileTitle = null;
+        public int maxFileTitle = 0;
+        public string initialDir = null;
+        public string title = null;
+        public int flags = 0;
+        public short fileOffset = 0;
+        public short fileExtension = 0;
+        public string defExt = null;
+        public IntPtr custData = IntPtr.Zero;
+        public IntPtr hook = IntPtr.Zero;
+        public string templateName = null;
+        public IntPtr reservedPtr = IntPtr.Zero;
+        public int reservedInt = 0;
+        public int flagsEx = 0;
     }
-    private void PlayGame()
+
+    [DllImport("comdlg32.dll", CharSet = CharSet.Auto)]
+    private static extern bool GetOpenFileName([In, Out] OpenFileName ofn);
+
+    private string OpenFileDialog(string title, string filter)
     {
-        SceneManager.LoadScene("Game");
+        OpenFileName ofn = new OpenFileName();
+        ofn.structSize = Marshal.SizeOf(ofn);
+        ofn.filter = filter;
+        ofn.file = new string(new char[256]);
+        ofn.maxFile = ofn.file.Length;
+        ofn.fileTitle = new string(new char[64]);
+        ofn.maxFileTitle = ofn.fileTitle.Length;
+        ofn.initialDir = Path.Combine(Application.dataPath, "MidiFiles");
+        ofn.title = title;
+        ofn.defExt = "mid";
+
+        if (GetOpenFileName(ofn))
+        {
+            return ofn.file;
+        }
+        return null;
     }
 }
-
